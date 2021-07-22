4e76ee510ad8b98e25bc95405c5c36cefbbebf6d
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-23 01:05:13 +0300

    Brew formula update for git-faker version v0.0.7

--- Formula/git-faker.rb
+++ Formula/git-faker.rb
@@ -5,28 +5,28 @@
 class GitFaker < Formula
   desc ""
   homepage ""
-  version "0.0.6"
+  version "0.0.7"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "http://github.com/danieltaub96/git-faker/releases/v0.0.6/git-faker_0.0.6_darwin_amd64.tar.gz", :using => CurlDownloadStrategy.
-      sha256 "27d9e141b5be2bfc608bd9f93e7044a8acf15ed014979096330ff3632952cb86"
+      url "http://github.com/danieltaub96/git-faker/releases/v0.0.7/git-faker_0.0.7_darwin_amd64.tar.gz", :using => GitHubGitDownloadStrategy.
+      sha256 "9e1486e24d0b8dff48529a0a59318e861d5dd36098a027bf6953c15d4d093649"
     end
     if Hardware::CPU.arm?
-      url "http://github.com/danieltaub96/git-faker/releases/v0.0.6/git-faker_0.0.6_darwin_arm64.tar.gz", :using => CurlDownloadStrategy.
-      sha256 "7db0de4a43f08d8b52b84d37750de97404c3339fea110ae7b875573adb2498ef"
+      url "http://github.com/danieltaub96/git-faker/releases/v0.0.7/git-faker_0.0.7_darwin_arm64.tar.gz", :using => GitHubGitDownloadStrategy.
+      sha256 "20517d62756c108b05abff608f25a2e14c6874f3432fd010a898082f91fe3257"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "http://github.com/danieltaub96/git-faker/releases/v0.0.6/git-faker_0.0.6_linux_amd64.tar.gz", :using => CurlDownloadStrategy.
-      sha256 "bb9c0293ae92d1afb91e8e48dccf80496f0764308dfd3f1ceea2aad0f746e786"
+      url "http://github.com/danieltaub96/git-faker/releases/v0.0.7/git-faker_0.0.7_linux_amd64.tar.gz", :using => GitHubGitDownloadStrategy.
+      sha256 "713b78d417760dc4af29c98f0fdddd1fd04b73326024406e739e35d66c923b1a"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "http://github.com/danieltaub96/git-faker/releases/v0.0.6/git-faker_0.0.6_linux_arm64.tar.gz", :using => CurlDownloadStrategy.
-      sha256 "f4de52927bc6d05822815b5ad5d99292678cebb2107565eadf1ff5ff39bed664"
+      url "http://github.com/danieltaub96/git-faker/releases/v0.0.7/git-faker_0.0.7_linux_arm64.tar.gz", :using => GitHubGitDownloadStrategy.
+      sha256 "39c667ad1214f595b2493b223f717d9c8bdf135eef71785a9f09f1202cca28fa"
     end
   end
 
